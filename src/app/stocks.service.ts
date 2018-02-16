130d21e731788adbdb3e509521e53110332e809a
tom <you@example.com>
2018-02-16 14:41:35 +0100

    Save the stock data with the HTML5 standard database LocalStorage

--- src/app/stocks.service.ts
+++ src/app/stocks.service.ts
@@ -4,13 +4,15 @@ import { HttpClient } from '@angular/common/http';
 @Injectable()
 export class StocksService {
 
-  stockNames: string[] = ['dax'];
+  stockNames: string[] = this.getStockNames();
   refreshStocksEvent = new EventEmitter();
 
   constructor(private httpClient: HttpClient) { }
 
   addNewStock(name: string) {
+    this.stockNames = this.getStockNames();
     this.stockNames.push(name);
+    this.saveStockNames();
   }
 
   getCurrentStockQuotas() {
@@ -19,6 +21,24 @@ export class StocksService {
 
   refreshStocks() {
     this.refreshStocksEvent.emit();
+  }
+
+  private getStockNames() {
+
+    let eintraegeArray;
+    const eintraegeText = localStorage.getItem('stockNames');
+    if (!eintraegeText) {
+      const leererEintraegeArray = [];
+      localStorage.setItem('stockNames', JSON.stringify(leererEintraegeArray));
+    } else {
+      eintraegeArray = JSON.parse(eintraegeText);
+    }
+    return eintraegeArray as string[];
+  }
+
+  private saveStockNames() {
+    const eintraegeText = JSON.stringify(this.stockNames);
+    localStorage.setItem('stockNames', eintraegeText);
   }
 
 }
